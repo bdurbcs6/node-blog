e7e1fe49da138c543d65261af47fa1b637670048
bdurb <brian@rpmgreenville.com>
2018-08-01 15:06:44 -0700

    set up get route for users

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const server = express();
 const userdb = require('./data/helpers/userDb');
 const postdb = require('./data/helpers/postDb');
 const tagdb = require('./data/helpers/tagDb');
 
 const PORT = 8000;
 
-const server = express();
-
 server.use(express.json());
+
+server.get('/users', (req, res) => {
+  userdb.get()
+  .then(users => 
+    res.status(200).json(users)
+  )
+  .catch(() => {
+    res.status(500).json({error: "you gots an error"})
+  })
+})
 
 
 server.listen(PORT, () => {
