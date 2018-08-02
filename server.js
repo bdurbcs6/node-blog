0664ce05e05e0f576a714ba06e1c4553cdcd98b2
bdurb <brian@rpmgreenville.com>
2018-08-01 19:48:23 -0700

    completed all routes for user

--- server.js
+++ server.js
@@ -19,6 +19,77 @@ server.get('/users', (req, res) => {
   })
 })
 
+server.get('/users/:id', (req, res) => {
+  const {id} = req.params
+  userdb.get(id)
+  .then(user => {
+    if (!user) {
+      res.status(404).json({error: "That user cannot be found"})
+    } else {
+      res.status(200).json(user)
+    }
+  })
+  .catch(() => {
+    res.status(500).json({error: "you gots an error"})
+  })
+})
+
+server.post('/users', (req, res) => {
+  const user = req.body
+  if (!user.name) {
+    res.status(400).json({error: "Gots to give us a name"})
+  }
+  userdb.insert(user)
+  .then(id => {
+    res.status(200).send(id)
+  })
+  .catch(() => {
+    res.status(500).json({error: "could not remove"})
+  })
+})
+
+server.delete('/users/:id', (req, res) => {
+  const {id} = req.params
+  userdb.remove(id)
+  .then(deleted => {
+    if (deleted != 1) {
+      res.status(400).json({error: "no user yo"})
+    } else {
+      res.status(200).json({message: "User was deleted"})
+    }
+  })
+  .catch(() => {
+    res.status(500).json({error: "epic fail"})
+  })
+})
+
+server.put('/users/:id', (req, res) => {
+  const {id} = req.params
+  const user = req.body
+  userdb.update(id, user)
+  .then(updated => {
+    if (updated != 1) {
+      res.status(400).json({message: "nope"})
+    } else {
+      res.status(200).json({message: "user updated"})
+    }
+  })
+  .catch(() => {
+    res.status(500).json({message: "User was updated!"})
+  })
+})
+
+server.get('/users/posts/:id', (req, res) => {
+  const {id} = req.params
+  userdb.getUserPosts(id)
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(() => {
+    res.status(500).json({message: "Danger Will Robinson"})
+  })
+})
+
 
 // posts-----------------------------
 
